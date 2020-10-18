8089dba662c49ee1e8e45bd76cbd152ca4b7474e
Algoud Marek <marek.algoud@gmail.com>
2020-10-18 16:17:29 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import * as THREE from 'three'
+// import ReactDOM from 'react-dom'
+import React from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+import { Physics, usePlane, useBox } from '@react-three/cannon'
+import { OrbitControls } from '@react-three/drei'
 
-function App() {
+function Plane(props) {
+  const [ref] = usePlane(() => ({ mass: 0, ...props }))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[5, 5]} />
+      <shadowMaterial attach="material" color="#171717" opacity={0.5} />
+    </mesh>
+  )
 }
 
-export default App;
+function Cube({ position, color }) {
+  const [ref, api] = useBox(() => ({
+    mass: 1000,
+    args: [1, 1, 1],
+    position
+  }))
+
+  // useFrame(({ clock }) => api.rotation.set(Math.sin(clock.getElapsedTime()), 0, 0))
+
+
+  return (
+    <mesh receiveShadow castShadow ref={ref}  args={[1, 1, 1]}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color={color} />
+    </mesh>
+  )
+}
+const w = '#ffffff'
+const r = '#FF0000'
+const m = '#9C4205'
+const n = '#000000' 
+const b = '#294BBF'
+const y = '#E8FF2D'
+const p = '#FFC590'
+const mario = [
+  [w,w,w,w,w,w,w,w,w,w,w,w,w],
+  [w,w,w,r,r,r,r,r,r,w,w,w,w],
+  [w,w,r,r,r,r,r,r,r,r,r,r,w],
+  [w,w,m,m,m,p,p,p,n,p,w,w,w],
+  [w,m,p,m,p,p,p,p,n,p,p,p,w],
+  [w,m,p,m,m,p,p,p,p,n,p,p,p],
+  [w,m,m,p,p,p,p,p,n,n,n,n,w],
+  [w,w,w,p,p,p,p,p,p,p,p,w,w],
+  [w,w,r,r,b,r,r,r,r,w,w,w,w],
+  [w,r,r,r,b,r,r,b,r,r,r,w,w],
+  [r,r,r,r,b,b,b,b,r,r,r,r,w],
+  [p,p,r,b,y,b,b,y,b,r,p,p,w],
+  [p,p,p,b,b,b,b,b,b,p,p,p,w],
+  [p,p,b,b,b,b,b,b,b,b,p,p,w],
+  [w,w,b,b,b,w,w,b,b,b,w,w,w],
+  [w,m,m,m,w,w,w,w,m,m,m,w,w],
+  [m,m,m,m,w,w,w,w,m,m,m,m,w],
+]
+
+const MarioCube = mario.map((line, x) => {
+  return line.map((color, y) => {
+    return (
+      <Cube receiveShadow castShadow position={[y * 1.2 - ((line.length / 2) * 1.2), Math.random() * 50, x * 1.2 - ((mario.length / 2) * 1.2)]} color={color} />
+    )
+  })
+})
+
+export default function App() {
+  return (
+    <Canvas shadowMap colorManagement gl={{ alpha: false }} camera={{ position: [0, 60, 0], fov: 50 }}>
+      <color attach="background" args={['lightblue']} />
+      <hemisphereLight intensity={0.35} />
+      <spotLight position={[0, 100, 0]} angle={0.3} penumbra={1} intensity={1} castShadow shadow-mapSize-width={256} shadow-mapSize-height={256} />
+      <Physics gravity={[0, -40, 0]}>
+        <Plane rotation={[-Math.PI / 2, 0, 0]} />
+        {MarioCube}
+      </Physics>
+      <OrbitControls />
+    </Canvas>
+  )
+}
